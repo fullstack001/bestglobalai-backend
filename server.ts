2aa17bdbf42f8d8c130c16fe2b1f30168b017d80
FullStack001 <mr.dimas332@gmail.com>
2024-12-25 09:50:08 +0200

    cors fix

--- server.ts
+++ server.ts
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import { createServer } from "http";
-import { Server } from "socket.io";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import path from "path";
@@ -13,18 +11,22 @@ import bookRoutes from "./routes/bookRoutes";
 import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 import { createDefaultAdmin } from "./controllers/userController";
-// import chatRoutes from "./routes/chatRoutes";
 
 dotenv.config();
 
-if (!process.env.MONGO_URI || !process.env.PORT) {
+if (!process.env.MONGO_URI) {
   throw new Error("Required environment variables are missing.");
 }
 
 const app = express();
 
+// app.use(cors({ origin: "*" }));
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || "*", // Allow dynamic origins
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
-app.use(cors({ origin: "*" }));
 app.use(bodyParser.json({ limit: "900mb" }));
 app.use(bodyParser.urlencoded({ limit: "900mb", extended: true }));
 
@@ -33,18 +35,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/books", bookRoutes);
-// app.use("/api/chats", chatRoutes);
 
-
-// const httpServer = createServer(app);
-// const io = new Server(httpServer, {
-//     cors: {
-//         origin: "*",
-//     },
-   
-// });
-
-// setupSocket(io);
 
 app.get("/", (req, res) => {
   res.send("Server running");
@@ -68,10 +59,6 @@ mongoose
   .catch((err) => console.log("MongoDB connection error", err));
 
 const PORT = process.env.PORT || 5001;
-
-// httpServer.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
