155761d5c82d6a015784a106bfa8a3975f310ffa
FullStack001 <mr.dimas332@gmail.com>
2025-02-03 23:53:06 +0200

    service admin email send function added

--- controllers/serviceController.ts
+++ controllers/serviceController.ts
@@ -45,7 +45,7 @@ export const createServiceOrder = async (req: Request, res: Response) => {
 
   const adminData = {
     from: email,
-    to: 'jimmusgrave.jm@gmail.com',
+    to: process.env.ADMIN_EMAIL,
     subject: "Customer Selected APIs",
     text: `APIs:\n\n${formattedAPIs}\n\n`,  
   };
