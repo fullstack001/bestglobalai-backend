d94bca87e19408c174ccd4eaf7febe079ec8a24d
fullstack001 <mr.dimas332@gmail.com>
2025-02-17 11:57:37 -0600

    fix role manage

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -5,22 +5,24 @@ import {
   deleteUser,
   updateProfile,
   getProfile,
-  changePassword
+  changePassword,
 } from "../controllers/userController";
 
 import verifyRole from "../middlewares/roleMiddleware";
-import { authenticateToken, authenticateUser } from "../middlewares/authMiddleware";
+import {
+  authenticateToken,
+  authenticateUser,
+} from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
 // Update user role (admin access only)
 
 router.get("/", getUsers);
-router.put("/role", verifyRole(["admin"]), updateUserRole);
+router.put("/role", verifyRole(["superAdmin"]), updateUserRole);
 router.delete("/:id", deleteUser);
-router.get('/profile', authenticateToken, getProfile);
-router.put('/profile', authenticateToken, updateProfile);
+router.get("/profile", authenticateToken, getProfile);
+router.put("/profile", authenticateToken, updateProfile);
 router.put("/change-password", authenticateUser, changePassword);
-
 
 export default router;
